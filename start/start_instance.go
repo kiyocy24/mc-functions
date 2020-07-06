f62512b4e4c3984cd48d0e22272301dcbaee892d
kiyocy24 <kiyocy24@gmail.com>
2020-07-06 23:49:43 +0900

    impl sample func

---
+++ start/start_instance.go
@@ -0,0 +1,19 @@
+package startinstance
+
+import (
+	"context"
+	"log"
+)
+
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+func StartInstance(ctx context.Context, m PubSubMessage) error {
+	name := string(m.Data)
+	if name == "" {
+		name = "daigo"
+	}
+	log.Printf("Hello, %s!", name)
+	return nil
+}
