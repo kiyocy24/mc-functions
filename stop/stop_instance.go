ad07b5b33df339b78c80e89e4c5660a80f62c990
kiyocy24 <kiyocy24@gmail.com>
2020-07-16 00:04:26 +0900

    add stop functions

---
+++ stop/stop_instance.go
@@ -0,0 +1,42 @@
+package stopinstance
+
+import (
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
+)
+
+var (
+	project  = os.Getenv("PROJECT")
+	zone     = os.Getenv("ZONE")
+	instance = os.Getenv("INSTANCE")
+)
+
+// PubSubMessage pub/sub message type
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// StopInstance stop instance
+func StopInstance(ctx context.Context, m PubSubMessage) error {
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := computeService.Instances.Stop(project, zone, instance).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v\n", resp)
+	return nil
+}
